37ddd3f19272b7d5fe07e317464f06f4752ae8fe
Roderic Page <rdmpage@gmail.com>
2014-04-29 16:49:04 +0100

    Bulk upload via API

--- api/fetch.php
+++ api/fetch.php
@@ -3,29 +3,34 @@
 require_once (dirname(dirname(__FILE__)) . '/couchsimple.php');
 require_once (dirname(dirname(__FILE__)) . '/lib.php');
 
-
+// Specimens and sequences
 $url = 'http://www.boldsystems.org/index.php/API_Public/combined';
 
 $url .= '?container=' . 'MBMIA';
 $url .= '&combined_download=tsv';
 
-//echo $url . "\n";
 
 $data = get($url);
 
-//echo $data;
-
 if ($data)
 {
+	// CouchDB
+	$docs = new stdclass;
+	$docs->docs = array();
+	
+	$bulk_size = 1000;
+	$bulk_count = 0;
+	$bulk_override = false;
+
+
 	$lines = explode("\n", $data);
 
 	$keys = array();
 	$row_count = 0;
-
-
+	
 	foreach ($lines as $line)
 	{
-		//echo $line . "\n----\n";
+		if ($line == '') break;
 		$row = explode("\t", $line);
 		
 		if ($row_count == 0)
@@ -34,7 +39,6 @@ if ($data)
 		}
 		else
 		{
-			print_r($row);
 			
 			$obj = new stdclass;
 			
@@ -50,13 +54,50 @@ if ($data)
 			// id
 			$obj->_id = $obj->processid;
 			
-			//print_r($obj);
+			// Upload in bulk
+			$docs->docs [] = $obj;
+			echo ".";
 			
-			echo json_encode($obj);
+			if (count($docs->docs ) == $bulk_size)
+			{
+				if ($bulk_override)
+				{
+					$docs->new_edits = false;
+				}
+			
+				echo "CouchDB...";
+				$resp = $couch->send("POST", "/" . $config['couchdb_options']['database'] . '/_bulk_docs', json_encode($docs));
+				$bulk_count += $bulk_size;
+				echo "\nUploaded... total=$bulk_count\n";
+			
+				$docs->docs  = array();
+			}
 		}
 		
 		$row_count++;
 	}
+	
+	 // Make sure we load the last set of docs
+	if (count($docs->docs ) != 0)
+	{
+		echo "CouchDB...\n";
+		
+		
+		if ($bulk_override)
+		{
+			$docs->new_edits = false;
+		}
+		
+		$resp = $couch->send("POST", "/" . $config['couchdb_options']['database'] . '/_bulk_docs', json_encode($docs));		
+		echo $resp;
+		
+		
+		$bulk_count += count($docs->docs);
+		echo "\nUploaded... total=$bulk_count\n";
+	
+	
+		$docs->docs  = array();
+	}	
 }
 	
 
